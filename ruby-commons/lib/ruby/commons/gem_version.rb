2207d8511ce36cd7042e188800806ad3f304750b
Alexander Bragin <alexander.bragin@gmail.com>
2016-07-12 10:54:35 +0300

    Preparing for [v0.1.0] release.

--- ruby-commons/lib/ruby/commons/gem_version.rb
+++ ruby-commons/lib/ruby/commons/gem_version.rb
@@ -7,8 +7,8 @@ module Ruby::Commons
 
   module VERSION
     MAJOR = 0
-    MINOR = 0
-    TINY  = 5
+    MINOR = 1
+    TINY  = 0
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
